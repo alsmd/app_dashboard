8c0cdf435995212548b8c5708ab264dff8ef18b3
Celebro01 <dangerveneta@gmail.com>
2020-10-19 11:51:58 -0300

    implementando o metodo $.ajax

--- App/Models/Dashboard.php
+++ App/Models/Dashboard.php
@@ -24,6 +24,8 @@ class Dashboard{
     public $data_fim;
     public $numeroVendas;
     public $totalVendas;
+    public $clientes_ativos;
+    public $clientes_inativos;
 
     public function __get($attr){
         return $attr;
@@ -60,17 +62,50 @@ class Bd{
 
         return $stmt->fetch(\PDO::FETCH_OBJ)->total_de_vendas;
     }
+
+    public function getClientesAtivos(){
+        $query = 'SELECT COUNT(*) as clientes_ativos FROM tb_clientes WHERE cliente_ativo = 1 ';
+        $stmt = $this->con->prepare($query);
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_OBJ)->clientes_ativos;
+    }
+    public function getClientesInativos(){
+        $query = 'SELECT COUNT(*) as clientes_inativos FROM tb_clientes WHERE cliente_ativo = 0 ';
+        $stmt = $this->con->prepare($query);
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_OBJ)->clientes_inativos;
+    }
+
 }
 
 $dashboard = new Dashboard();
-$dashboard->__set('data_inicio','2018-08-01');
-$dashboard->__set('data_fim','2018-08-31');
-$bd = new Bd($dashboard,Connection::getDb());
 
+//Criando dinamicamente o mes
+$competencia = explode('-',$_POST['competencia']) ;
+$ano = $competencia[0];
+$mes = $competencia[1];
+
+//veriicando quantidade de dias no respectivo mês
+$dias_do_mes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
+$data_inicio = "$ano-$mes-01";
+$data_fim = "$ano-$mes-$dias_do_mes";
+
+$dashboard->__set('data_inicio',$data_inicio);
+$dashboard->__set('data_fim',$data_fim);
+//******************** */
+
+$bd = new Bd($dashboard,Connection::getDb());
+//*Populando o objeto com os dados recuperados no bd
 $NDV = $bd->getNumeroVendas();
 $TDV = $bd->getTotalVendas();
+$CA = $bd->getClientesAtivos();
+$CI = $bd->getClientesInativos();
 $dashboard->__set('numeroVendas',$NDV);
 $dashboard->__set('totalVendas',$TDV);
+$dashboard->__set('clientes_ativos',$CA);
+$dashboard->__set('clientes_inativos',$CI);
 
 echo json_encode($dashboard);
 
--- public/src/js/script.js
+++ public/src/js/script.js
@@ -3,9 +3,11 @@ $(document).ready(() => {
     $("#home").on('click',()=>{
         $.post('src/pages/home.phtml',(data)=>{
             $("#pagina").html(data);
+            inserindoDados()
         })
     })
     $('#home').trigger('click');
+
     //documentacao
     $("#documentacao").on('click',()=>{
         $.post('src/pages/documentacao.phtml',(data)=>{
@@ -19,9 +21,34 @@ $(document).ready(() => {
         })
     })
     //Inserindo informações no Dash
-    $.post('src/dados.php',(data)=>{
-        $info = JSON.parse(data)
-        $("#numero_vendas").html($info.numeroVendas);
-        $("#total_vendas").html($info.totalVendas);
-    })
+    function inserindoDados(){
+        $("#competencia").on('change',function(e){
+            let data = $(e.target).val()
+            //recupera os dados
+            let $dados = $.ajax({
+                type: 'POST',
+                url: 'src/dados.php',
+                data: 'competencia='+data, //x-www-form-urlencoded
+                dataType: 'json',
+                success: (success)=>{ console.log(success)},
+                error:  (erro)=>{ console.log(erro)}
+
+            })
+            //metodo, url, dados, sucesso,erro
+            //renderiza os dados na tela
+            $dados.then((d)=>{
+                let $info = JSON.parse(d);
+                $("#numero_vendas").html($info.numeroVendas);
+                $("#total_vendas").html($info.totalVendas);
+                $("#clientes_ativos").html($info.clientes_ativos);
+                $("#clientes_inativos").html($info.clientes_inativos);
+            })
+        })
+        $("#competencia").trigger("change");
+
+    }
+    
+
+    
+
 })
